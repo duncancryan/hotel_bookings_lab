4038d2c86fc4156bde10429a9b718ab2cb9c5c38
Callum Wolfe <wolfe.callum@gmail.com>
2020-09-02 13:23:07 +0100

    Server.js file complete

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const parser = require("body-parser");
+const cors = require("cors");
+app.use(cors());
+app.use(parser.json());
+
+const MongoClient = require("mongodb").MongoClient;
+const createRouter = require("./helpers/create_router");
+
+MongoClient.connect("mongodb://localhost:27017")
+.then((client) => {
+    const db = client.db("hotel");
+    const bookingsCollection = db.collection("bookings");
+    const bookingsRouter = createRouter(bookingsCollection);
+    app.use("/api/bookings", bookingsRouter);
+})
+.catch(console.err);
+
+app.listen(3000, () => console.log("Listening on port 3000"));
