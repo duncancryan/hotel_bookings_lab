305619c43a1b1504e9d9fc4baf274bd2581044ea
Duncan Ryan <duncancryan1@gmail.com>
2020-09-02 14:43:17 +0100

    List coded and displaying html

--- client/src/components/BookingList.vue
+++ client/src/components/BookingList.vue
@@ -1,10 +1,18 @@
 <template>
-  
+  <div id="booking-list">
+      <h2>Bookings</h2>
+      <booking-item v-for="(booking, index) in bookings" :key="index" booking="booking"/>
+  </div>
 </template>
 
 <script>
+import BookingItem from './BookingItem';
 export default {
-
+    name: 'booking-list',
+    components: {
+        'booking-item': BookingItem
+    },
+    props: ['bookings']
 }
 </script>
 
