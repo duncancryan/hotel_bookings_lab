5c515ea2ee6849f18d86e1bc83c773aae249e3f6
Duncan Ryan <duncancryan1@gmail.com>
2020-09-02 13:28:13 +0100

    get function coded

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -5,5 +5,13 @@ const createRouter = function (collection) {
     
     const router = express.Router();
 
-    
+    router.get('/', (rew, res) => {
+      collection.find().toArray()
+      .then((docs) => res.json(docs))
+      .catch((err) => {
+        console.error(err);
+        res.status(500);
+        res.json({ status: 500, error: err });
+        });
+    });
 }
