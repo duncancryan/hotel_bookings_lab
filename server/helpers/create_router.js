8f3d08c312367bc3788f1b3af4b17cd20897748c
Duncan Ryan <duncancryan1@gmail.com>
2020-09-02 13:23:48 +0100

    set router as express Router

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -1,2 +1,9 @@
 const express = require('express');
 const ObjectID = require('mongodb').ObjectID;
+
+const createRouter = function (collection) {
+    
+    const router = express.Router();
+
+    
+}
