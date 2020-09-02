b3af83a8094589721783a6e4aac116589ced8f01
Duncan Ryan <duncancryan1@gmail.com>
2020-09-02 15:06:54 +0100

    form html written

--- client/src/components/BookingForm.vue
+++ client/src/components/BookingForm.vue
@@ -1,5 +1,17 @@
 <template>
-  
+  <form id="booking-form" v-on:submit.prevent="addBooking">
+      <h3>Add Booking:</h3>
+      <div class="formWrap">
+          <label for="name">Name: </label>
+          <input type="text" id="name" v-model="name" required/>
+      </div>
+      <div class="formWrap">
+          <label for="email">Email: </label>
+          <input type="email" id="email" v-model="email" required/>
+      </div>
+      <input type="submit" value="Add" id="save">
+
+  </form>
 </template>
 
 <script>
