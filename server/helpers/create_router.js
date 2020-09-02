b137b12ec66cf10194c38160798fd9ca5ffd88ee
Duncan Ryan <duncancryan1@gmail.com>
2020-09-02 13:31:10 +0100

    show function written

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -14,4 +14,15 @@ const createRouter = function (collection) {
         res.json({ status: 500, error: err });
         });
     });
+
+    router.get('/:id', (req, res) => {
+        const id = req.params.id;
+        collection.findOne({ _id: ObjectID(id) })
+        .then((doc) => res.json(doc))
+        .catch((err) => {
+            console.error(err);
+            res.status(500);
+            res.json({ status: 500, error: err });
+        });
+    });
 }
