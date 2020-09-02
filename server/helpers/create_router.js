c0892a00146ded975b99a91745aeb818f725143e
Duncan Ryan <duncancryan1@gmail.com>
2020-09-02 13:42:26 +0100

    merged, fixed typo

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -5,7 +5,7 @@ const createRouter = function (collection) {
 
     const router = express.Router();
 
-    router.get('/', (rew, res) => {
+    router.get('/', (req, res) => {
         collection.find().toArray()
             .then((docs) => res.json(docs))
             .catch((err) => {
